ff234513c66cbc357c45b0ae7e88f074491a03e3
Jackie Chen <jackielovu@163.com>
2019-09-30 19:35:40 +0800

    公制英制转换

---
+++ 0-100/metric_translation.py
@@ -0,0 +1,10 @@
+# 英制和公制转换
+
+v = float(input('请输入长度：'))
+unit = input('请输入单位：')
+if unit == 'in' or unit == '英寸':
+    print('%.10f英寸 = %.10f厘米' % (v, v * 2.54))
+elif unit == 'cm' or unit == '厘米':
+    print('%.10f厘米 = %.10f英寸' % (v, v / 2.54))
+else:
+    print('请输入有效的单位！')
