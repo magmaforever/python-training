fe9fd0f5b5082747c15f84345f54cdacbed5fc8c
Jackie Chen <jackielovu@163.com>
2019-09-30 19:36:41 +0800

    英制公制转换

--- 0-100/metric_translation.py
+++ 0-100/metric_translation.py
@@ -1,7 +1,7 @@
 # 英制和公制转换
 
 v = float(input('请输入长度：'))
-unit = input('请输入单位：')
+unit = input('请输入单位(英寸in或厘米cm))：')
 if unit == 'in' or unit == '英寸':
     print('%.10f英寸 = %.10f厘米' % (v, v * 2.54))
 elif unit == 'cm' or unit == '厘米':
