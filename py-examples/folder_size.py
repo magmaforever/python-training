ed3168c5dc5002d7a383824ae9a9ae43a1d642c3
Jackie Chen <jackielovu@163.com>
2019-09-30 19:25:22 +0800

    查看文件夹大小

--- py-examples/folder_size.py
+++ py-examples/folder_size.py
@@ -12,14 +12,16 @@ fsizedicr = {
     'Bytes': 1,
     'Kilobytes': float(1) / 1024,
     'Megabytes': float(1) / (1024 * 1024),
-    'Gigabytes': float(1)/(1024*1024*1024)
+    'Gigabytes': float(1) / (1024 * 1024 * 1024)
 }
-for(path, dirs, files) in os.walk(directory):
+for (path, dirs, files) in os.walk(directory):
     for file in files:
         filename = os.path.join(path, file)
         dir_size += os.path.getsize(filename)
 
-fsizeList = [str(round(fsizedicr[key] * dir_size, 2)) + " " + key for key in fsizedicr]
+fsizeList = [
+    str(round(fsizedicr[key] * dir_size, 2)) + " " + key for key in fsizedicr
+]
 
 if dir_size == 0:
     print("文件为空。")
