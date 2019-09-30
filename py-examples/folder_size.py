bbc72c180233a3b1ce23da478ca405a409f79b55
Jackie Chen <jackielovu@163.com>
2019-09-30 19:13:04 +0800

    取得文件夹大小

---
+++ py-examples/folder_size.py
@@ -0,0 +1,28 @@
+# check folder size
+
+import os
+import sys
+try:
+    directory = sys.argv[1]
+except IndexError:
+    sys.exit('Must provide an argument!')
+
+dir_size = 0
+fsizedicr = {
+    'Bytes': 1,
+    'Kilobytes': float(1) / 1024,
+    'Megabytes': float(1) / (1024 * 1024),
+    'Gigabytes': float(1)/(1024*1024*1024)
+}
+for(path, dirs, files) in os.walk(directory):
+    for file in files:
+        filename = os.path.join(path, file)
+        dir_size += os.path.getsize(filename)
+
+fsizeList = [str(round(fsizedicr[key] * dir_size, 2)) + " " + key for key in fsizedicr]
+
+if dir_size == 0:
+    print("文件为空。")
+else:
+    for units in sorted(fsizeList)[::-1]:
+        print("Folder Size:" + units)
